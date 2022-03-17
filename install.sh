26db5e3580ad4a8f19988a7fadca8642e88f39a0
Anderson Ibarra <anderson.ibarra@mercadolibre.com.co>
2022-03-16 22:37:19 -0500

    Implemented initial Menu

--- install.sh
+++ install.sh
@@ -7,3 +7,63 @@
 
 # This is a first version, and I hope it works well. 
 
+# Main Menu
+Menu(){
+    echo "Welcome to this set up script, please choose an option to select"\
+    "what you want to do."
+    
+    echo ""
+    echo "1) Creation of symbolic links"
+    echo "2) First step installation (Homebrew, git)"
+    echo "3) Install all the required packages for developing (without text editor)"
+    echo "4) Install and set up Neovim"
+    echo "5) Check compliances"
+    echo "6) Exit"
+    echo ""
+    echo "Please choose an option"
+}
+
+# Menu
+option=0
+until [ $option -eq 6 ]
+do
+    case $option in
+        1) 
+            clear
+            echo $option
+        	Menu
+            ;;
+        2)
+            clear
+            echo $option
+        	Menu
+            ;;
+        3)
+            clear
+            echo $option
+        	Menu
+            ;;
+        4)
+            clear
+            echo $option
+        	Menu
+            ;;
+        5)
+            clear
+            echo $option
+        	Menu
+            ;;
+        *)
+            clear
+        	Menu
+            ;;
+    esac
+    read option
+done
+
+if [ $option -eq 6 ]; then
+    clear
+fi
+
+#EOF
+
