f3fdccd7dd0b121703dc54972064817d1c784506
Anderson Ibarra <anderson.ibarra@mercadolibre.com.co>
2022-03-16 22:03:39 -0500

    Initial commit for the installation script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Script created by Anderson Ibarra
+
+# This Script is made to simplify the installation of the required tools
+# to be a web developer using Neovim, iterm2, zsh and tmux. 
+
+# This is a first version, and I hope it works well. 
+
