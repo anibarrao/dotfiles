bc70ce6f281532161478b95e5cc44153948b9233
anibarrao <anderson.ibarra@mercadolibre.com.co>
2022-03-18 13:04:32 -0500

    Implemented function to install tmux and fixed identation and nummeration in the menu

--- install.sh
+++ install.sh
@@ -12,6 +12,14 @@ DOTFILES=$HOME/.dotfiles
 EMAIL=anderson.ibarra@mercadolibre.com.co
 NAME=anibarrao
 
+# Install tmux
+Install_Tmux(){
+  echo "Installing tmux..."
+  sleep 0.5
+  echo ""
+  brew install tmux
+}
+
 # Install iterm2
 Install_iTerm2(){
   echo "Installing iterm2..."
@@ -57,6 +65,7 @@ symbolic_links(){
     echo "Creating symbolic links for zshrc"
     if [ -f $DOTFILES/zsh/zshrc ]; then
         ln -nfs $DOTFILES/zsh/zshrc $HOME/.zshrc
+        source ~/.zshrc
     fi
     echo "Removing previous configuration files for nvim"
     rm -rf $HOME/.config/nvim 
@@ -78,12 +87,13 @@ Menu(){
     echo "2) Install git"
     echo "3) Install Neovim"
     echo "4) Install iterm2"
-    echo "5) Creation of symbolic links"
-    echo "6) First step installation (Homebrew, git)"
-    echo "7) Install all the required packages for developing (without text editor)"
-    echo "8) Install and set up Neovim"
-    echo "9) Check compliances"
-    echo "10) Exit"
+    echo "5) Install tmux"
+    echo "6) Creation of symbolic links"
+    echo "7) First step installation (Homebrew, git)"
+    echo "8) Install all the required packages for developing (without text editor)"
+    echo "9) Install and set up Neovim"
+    echo "10) Check compliances"
+    echo "11) Exit"
     echo ""
     echo "Please choose an option"
 }
@@ -110,22 +120,24 @@ do
             Menu
             ;;
         4)
-          Install_iTerm2
-          sleep 0.5
-          clear Menu
-          ;;
+            Install_iTerm2
+            sleep 0.5
+            clear
+            Menu
+            ;;
         5)
+            Install_Tmux
+            sleep 0.5
+            clear
+            Menu
+            ;;
+        6)
             symbolic_links
             sleep 1
             clear
             echo "Symbolink links created"
             sleep 1
             clear
-        	Menu
-            ;;
-        6)
-            clear
-            echo $option
         	Menu
             ;;
         7)
@@ -139,6 +151,11 @@ do
         	Menu
             ;;
         9)
+            clear
+            echo $option
+        	Menu
+            ;;
+        10)
             clear
             echo $option
         	Menu
